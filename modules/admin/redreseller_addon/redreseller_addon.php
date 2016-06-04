da57964c77c33263a8b38f06843838ed95459201
Mostafa Amiri <amiri27@gmail.com>
2016-06-04 12:12:42 +0430

    Applied fixes from StyleCI (#2)

--- modules/admin/redreseller_addon/redreseller_addon.php
+++ modules/admin/redreseller_addon/redreseller_addon.php
@@ -1,19 +1,19 @@
 <?php
-	if (!defined('WHMCS')) {
-		exit('This file cannot be accessed directly');
-	}
+    if (!defined('WHMCS')) {
+        exit('This file cannot be accessed directly');
+    }
 
     $maxRows_log = 20;
     $pageNum_log = 0;
-		
+
     if (isset($_GET['page'])) {
         $pageNum_log = $_GET['page'] - 1;
     }
-    
-    $startRow_log    = $pageNum_log * $maxRows_log;
-    $query_log       = 'SELECT * FROM redreseller_log ORDER BY id DESC';
+
+    $startRow_log = $pageNum_log * $maxRows_log;
+    $query_log = 'SELECT * FROM redreseller_log ORDER BY id DESC';
     $query_limit_log = sprintf('%s LIMIT %d, %d', $query_log, $startRow_log, $maxRows_log);
-    
+
     if (!($log = @mysql_query($query_limit_log))) {
         mysql_query('CREATE TABLE IF NOT EXISTS `redreseller_log` (
           `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -29,9 +29,9 @@
           PRIMARY KEY (`id`)
         ) ENGINE=MyISAM');
     }
-    
-    $all_log        = @mysql_query($query_log);
-    $totalRows_log  = @mysql_num_rows($all_log);
+
+    $all_log = @mysql_query($query_log);
+    $totalRows_log = @mysql_num_rows($all_log);
     $totalPages_log = ceil($totalRows_log / $maxRows_log) - 1;
     echo '
     <style>
@@ -62,61 +62,62 @@
     <fieldset>
         <legend>دامین های ثبت شده</legend>
     ';
-    
+
     if ($totalRows_log == 0) {
         echo 'فاقد فعاليت';
+
         return 1;
     }
-    
-    
+
+
     if (0 < $pageNum_log) {
-        echo '<a href="addonmodules.php?module=redreseller_addon&logs&page=' . (max(0, $pageNum_log - 1) + 1) . '">صفحه قبل</a> | ';
+        echo '<a href="addonmodules.php?module=redreseller_addon&logs&page='.(max(0, $pageNum_log - 1) + 1).'">صفحه قبل</a> | ';
     }
-    
+
     if (1 < $totalPages_log) {
         for ($i = 0; $i <= $totalPages_log; $i++) {
             $abc = $i + 1;
             if ($pageNum_log == $i) {
-                echo '(' . $abc . ') ';
+                echo '('.$abc.') ';
             } else {
-                echo '<a href=\'addonmodules.php?module=redreseller_addon&logs&page=' . $abc . '\'>' . $abc . '</a> ';
+                echo '<a href=\'addonmodules.php?module=redreseller_addon&logs&page='.$abc.'\'>'.$abc.'</a> ';
             }
         }
     }
-    
+
     if ($pageNum_log < $totalPages_log) {
-        echo '| <a href="addonmodules.php?module=redreseller_addon&logs&page=' . (min($totalPages_log, $pageNum_log + 1) + 1) . '">صفحه بعد</a>';
+        echo '| <a href="addonmodules.php?module=redreseller_addon&logs&page='.(min($totalPages_log, $pageNum_log + 1) + 1).'">صفحه بعد</a>';
     }
-    
+
     echo '<table width=\'70%\'>'
-        . '<tr>'
-        . '<td><b>دامین</b></td><td><b>ایمیل</b></td><td><b>نیک هندل</b></td><td><b>مدت</b></td><td><b>ns1</b></td><td><b>ns2</b></td><td><b>نوع</b></td><td><b>نتیجه</b></td><td><b>زمان</b></td>'
-        . '</tr>';
+        .'<tr>'
+        .'<td><b>دامین</b></td><td><b>ایمیل</b></td><td><b>نیک هندل</b></td><td><b>مدت</b></td><td><b>ns1</b></td><td><b>ns2</b></td><td><b>نوع</b></td><td><b>نتیجه</b></td><td><b>زمان</b></td>'
+        .'</tr>';
     while ($row_log = mysql_fetch_assoc($log)) {
         echo '<tr>'
-            . '<td style=\'border:1px solid black;\'>' . $row_log['domain'] . '</td><td style=\'border:1px solid black;\'>' . $row_log['email'] . '</td><td style=\'border:1px solid black;\'>' . $row_log['handle'] . '</td><td style=\'border:1px solid black;\'>' . $row_log['duration'] . '</td><td style=\'border:1px solid black;\'>' . $row_log['ns1'] . '</td><td style=\'border:1px solid black;\'>' . $row_log['ns2'] . '</td><td style=\'border:1px solid black;\'>' . $row_log['result'] . '</td><td style=\'border:1px solid black;\'>' . $row_log['type'] . '</td>'
-            . '<td style=\'border:1px solid black;\'>' . date('Y-m-d, G:i', $row_log['date']) . '</td>'
-            . '</tr>';
+            .'<td style=\'border:1px solid black;\'>'.$row_log['domain'].'</td><td style=\'border:1px solid black;\'>'.$row_log['email'].'</td><td style=\'border:1px solid black;\'>'.$row_log['handle'].'</td><td style=\'border:1px solid black;\'>'.$row_log['duration'].'</td><td style=\'border:1px solid black;\'>'.$row_log['ns1'].'</td><td style=\'border:1px solid black;\'>'.$row_log['ns2'].'</td><td style=\'border:1px solid black;\'>'.$row_log['result'].'</td><td style=\'border:1px solid black;\'>'.$row_log['type'].'</td>'
+            .'<td style=\'border:1px solid black;\'>'.date('Y-m-d, G:i', $row_log['date']).'</td>'
+            .'</tr>';
     }
-    
+
     echo '</table><br />';
-    
+
     if (0 < $pageNum_log) {
-        echo '<a href="addonmodules.php?module=redreseller_addon&logs&page=' . (max(0, $pageNum_log - 1) + 1) . '">صفحه قبل</a> | ';
+        echo '<a href="addonmodules.php?module=redreseller_addon&logs&page='.(max(0, $pageNum_log - 1) + 1).'">صفحه قبل</a> | ';
     }
-    
+
     if (1 < $totalPages_log) {
         for ($i = 0; $i <= $totalPages_log; $i++) {
             $abc = $i + 1;
             if ($pageNum_log == $i) {
-                echo '(' . $abc . ') ';
+                echo '('.$abc.') ';
             } else {
-                echo '<a href=\'addonmodules.php?module=redreseller_addon&logs&page=' . $abc . '\'>' . $abc . '</a> ';
+                echo '<a href=\'addonmodules.php?module=redreseller_addon&logs&page='.$abc.'\'>'.$abc.'</a> ';
             }
         }
     }
     if ($pageNum_log < $totalPages_log) {
-        echo '| <a href="addonmodules.php?module=redreseller_addon&logs&page=' . (min($totalPages_log, $pageNum_log + 1) + 1) . '">صفحه بعد</a><br /><br />';
+        echo '| <a href="addonmodules.php?module=redreseller_addon&logs&page='.(min($totalPages_log, $pageNum_log + 1) + 1).'">صفحه بعد</a><br /><br />';
     }
+
     return 1;
-?>
--- modules/registrars/redreseller/redreseller.php
+++ modules/registrars/redreseller/redreseller.php
@@ -1,123 +1,130 @@
 <?php
 
-    function redreseller_getConfigArray() {
-        $configarray = array(
-            'Webservice_id' => array(
-                'Type' => 'text', 'Size' => '36', 'Description' => 'Enter webservice id here'
-            ),
-            'Webservice_pass' => array(
-                'Type' => 'text', 'Size' => '36', 'Description' => 'Enter webservice password here'
-            ),
-            'TestMode' => array(
-                'Type' => 'yesno'
-            )
-        );
+    function redreseller_getConfigArray()
+    {
+        $configarray = [
+            'Webservice_id' => [
+                'Type' => 'text', 'Size' => '36', 'Description' => 'Enter webservice id here',
+            ],
+            'Webservice_pass' => [
+                'Type' => 'text', 'Size' => '36', 'Description' => 'Enter webservice password here',
+            ],
+            'TestMode' => [
+                'Type' => 'yesno',
+            ],
+        ];
 
         return $configarray;
     }
 
-    function redreseller_RegisterDomain($params) {
-        $domain = $params['sld'] . '.' . $params['tld'];
+    function redreseller_RegisterDomain($params)
+    {
+        $domain = $params['sld'].'.'.$params['tld'];
         $handle = $params['additionalfields']['NIC Handle'];
-        $duration = (int)$params['regperiod'] * 12;
+        $duration = (int) $params['regperiod'] * 12;
         $testmode = $params['TestMode'];
 
         $client = new SoapClient('http://www.redreseller.com/WebService/wsdl');
         $res = $client->PayDomain(
-            array('webservice_id' => $params['Webservice_id'], 'webservice_pass' => $params['Webservice_pass']),
+            ['webservice_id' => $params['Webservice_id'], 'webservice_pass' => $params['Webservice_pass']],
             $domain,
             $handle,
             $duration,
-            array(
+            [
                 'ns1' => $params['ns1'],
                 'ns2' => $params['ns2'],
                 'ns3' => $params['ns3'],
-                'ns4' => $params['ns4']
-            )
-        );	
+                'ns4' => $params['ns4'],
+            ]
+        );
 
-        mysql_query("INSERT INTO `redreseller_log`(`domain`,`email`,`handle`,`duration`,`ns1`,`ns2`,`type`,`result`,`date`) VALUES ('" . $domain . "','" . $params['email'] . "','" . $handle . "','" . $duration . "','" . $ns1 . "','" . $ns2 . "','ثبت','" . $res . "','" . time() . "')");
-        
+        mysql_query("INSERT INTO `redreseller_log`(`domain`,`email`,`handle`,`duration`,`ns1`,`ns2`,`type`,`result`,`date`) VALUES ('".$domain."','".$params['email']."','".$handle."','".$duration."','".$ns1."','".$ns2."','ثبت','".$res."','".time()."')");
+
         if ($testmode == 'on') {
-            if ((int)$res != 1001) {
-                $values['error'] = 'error number = ' . $res;
+            if ((int) $res != 1001) {
+                $values['error'] = 'error number = '.$res;
             }
         } else {
-            if ((int)$res != 1000) {
-                $values['error'] = 'error number = ' . $res;
+            if ((int) $res != 1000) {
+                $values['error'] = 'error number = '.$res;
             }
         }
+
         return $values;
     }
 
-    function redreseller_RenewDomain($params) {
-        $domain = $params['sld'] . '.' . $params['tld'];
-        $duration = (int)$params['regperiod'] * 12;
+    function redreseller_RenewDomain($params)
+    {
+        $domain = $params['sld'].'.'.$params['tld'];
+        $duration = (int) $params['regperiod'] * 12;
         $testmode = $params['TestMode'];
 
-        $result = mysql_query('select tblclients.email from tbldomains,tblclients where tbldomains.domain=\'' . $domain . '\'');
+        $result = mysql_query('select tblclients.email from tbldomains,tblclients where tbldomains.domain=\''.$domain.'\'');
 
         $row = mysql_fetch_row($result);
         $email = $row[0];
 
         $client = new SoapClient('http://www.redreseller.com/WebService/wsdl');
         $res = $client->PayDomain(
-            array('webservice_id' => $params['Webservice_id'], 'webservice_pass' => $params['Webservice_pass']),
+            ['webservice_id' => $params['Webservice_id'], 'webservice_pass' => $params['Webservice_pass']],
             $domain,
             $handle,
             $duration,
-            array(
+            [
                 'ns1' => '',
                 'ns2' => '',
                 'ns3' => '',
-                'ns4' => ''
-            )
+                'ns4' => '',
+            ]
         );
 
-        mysql_query("INSERT INTO `redreseller_log`(`domain`,`email`,`handle`,`duration`,`ns1`,`ns2`,`type`,`result`,`date`) VALUES ('" . $domain . "','" . $params['email'] . "','" . $handle . "','" . $duration . "','" . $ns1 . "','" . $ns2 . "','تمدید','" . $res . "','" . time() . "')");
+        mysql_query("INSERT INTO `redreseller_log`(`domain`,`email`,`handle`,`duration`,`ns1`,`ns2`,`type`,`result`,`date`) VALUES ('".$domain."','".$params['email']."','".$handle."','".$duration."','".$ns1."','".$ns2."','تمدید','".$res."','".time()."')");
 
         if ($testmode == 'on') {
-            if ((int)$res != 1001) {
-                $values['error'] = 'error number = ' . $res;
+            if ((int) $res != 1001) {
+                $values['error'] = 'error number = '.$res;
             }
         } else {
-            if ((int)$res != 1000) {
-                $values['error'] = 'error number = ' . $res;
+            if ((int) $res != 1000) {
+                $values['error'] = 'error number = '.$res;
             }
         }
 
         return $values;
     }
-    
-    function redreseller_GetNameservers($params) {
-        $domain = $params['sld'] . '.' . $params['tld'];
-        
+
+    function redreseller_GetNameservers($params)
+    {
+        $domain = $params['sld'].'.'.$params['tld'];
+
         $client = new SoapClient('http://www.redreseller.com/WebService/wsdl');
         $res = $client->DomainInfo(
-            array('webservice_id' => $params['Webservice_id'], 'webservice_pass' => $params['Webservice_pass']),
+            ['webservice_id' => $params['Webservice_id'], 'webservice_pass' => $params['Webservice_pass']],
             $domain
         );
-        $values = array();
+        $values = [];
         if (!is_numeric($res)) {
             for ($i = 0; $i < count($res['domain']['dns']); $i++) {
-                $values['ns' . ($i+1)] = $res['domain']['dns'][$i];
+                $values['ns'.($i + 1)] = $res['domain']['dns'][$i];
             }
         } else {
-            $values['error'] = 'error number = ' . $res;
+            $values['error'] = 'error number = '.$res;
         }
+
         return $values;
     }
 
-    function redreseller_Sync($params) {
-        $domain = $params['sld'] . '.' . $params['tld'];
-        
+    function redreseller_Sync($params)
+    {
+        $domain = $params['sld'].'.'.$params['tld'];
+
         $client = new SoapClient('http://www.redreseller.com/WebService/wsdl');
         $res = $client->DomainInfo(
-            array('webservice_id' => $params['Webservice_id'], 'webservice_pass' => $params['Webservice_pass']),
+            ['webservice_id' => $params['Webservice_id'], 'webservice_pass' => $params['Webservice_pass']],
             $domain
         );
-        
-        $values = array();
+
+        $values = [];
         if (!is_numeric($res)) {
             $res = json_decode($res, true);
             if (!empty($res['domain']['expireDate'])) {
@@ -129,9 +136,8 @@
                 } else {
                     $values['expired'] = true;
                 }
-            }	
+            }
         }
+
         return $values;
     }
-
-?>
